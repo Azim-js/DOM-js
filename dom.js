a45412936b798a4a3b71d07aed98d66c1df4523f
azim <az1234im@gmail.com>
2021-05-15 17:03:44 +0530

    examining the dom

--- dom.js
+++ dom.js
@@ -1,1 +1,23 @@
 console.log("ALL ABOUT DOM");
+
+// Examining the Document OBJECT
+
+console.dir(document);
+
+console.log(document.URL);
+console.log(document.title);
+
+// document.title=123;
+
+console.log(document.doctype);
+console.log(document.head);
+console.log(document.body);
+console.log(document.all);
+console.log(document.all[10]);
+
+// document.all[10].textContent="Hello";
+
+console.log(document.forms);
+console.log(document.links);
+console.log(document.forms[0]);
+
