bb7254837f47090c260baf9613d69b7adbdf96e1
azim <az1234im@gmail.com>
2021-05-15 20:54:45 +0530

    getElementByClassName

--- dom.js
+++ dom.js
@@ -41,3 +41,23 @@ var header=document.getElementById("main-header");
 
 header.style.borderBottom='solid 3px #000';
 
+
+// GET ELEMENT BY CLASS NAME 
+
+var items=document.getElementsByClassName('list-group-item');
+console.log(items);
+
+console.log(items[1]);
+
+items[1].style.fontWeight='bold';
+
+// items[1].style.backgroundColor='yellow'; 
+
+// items[1].style.backgroundColor='#f4f4f4';
+
+
+for(var i=0;i<items.length;i++){
+    items[i].style.backgroundColor='#f4f4f4';
+}
+
+
